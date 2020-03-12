ba84f9133d3c12e2f01f275d837b35af25bbfebd
Caitlin Hinshaw <caitlin.hinshaw@gmail.com>
2020-03-12 02:20:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,17 @@
+# def reverse_each_word(string)
+#   array = string.split(" ")
+#   reverse_array = []
+#   array.each do |word|
+#     reverse_array << word.reverse
+#   end
+#   reverse_array.join(" ")
+# end
+
+
 def reverse_each_word(string)
   array = string.split(" ")
   reverse_array = []
-  array.each do |word|
+  array.collect do |word|
     reverse_array << word.reverse
   end
   reverse_array.join(" ")
